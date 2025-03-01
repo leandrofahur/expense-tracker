5d8b1539e68994a919e831fb15fa91f2764044dc
Leandro Miranda Fahur Machado <fahurleandro@gmail.com>
2025-03-01 15:03:00 -0800

    added the phosphor-icons for react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Money, CurrencyDollar } from "@phosphor-icons/react";
+
 function App() {
   return (
     <div className="flex w-screen h-screen text-3xl font-bold text-emerald-700 text-center justify-center items-center">
-      Expen$e Tracker
+      <CurrencyDollar size={32} />
+      <Money size={32} />
     </div>
   );
 }
